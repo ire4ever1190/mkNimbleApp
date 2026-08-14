d2eaf3662c8ab781f97101a00a8242a4bd13a285
Jake Leahy <jake@leahy.dev>
2026-08-14 20:58:12 +1000

    Update the patch phase to include the certificates
    Bit jank, need to make sure it doesn't trigger when the user hasn't changed the certificate
    
    Also need to match it just patch the upstream nimble package

--- nix/pkgs/nimble.nix
+++ nix/pkgs/nimble.nix
@@ -16,6 +16,21 @@ mkNimbleApp rec {
     hash = "sha256-39d9EsS0opz6vQzSE91gBRQbaTPeebVQLf/QdJoaD8o=";
     fetchSubmodules = true;
   };
+
+  patchPhase = ''
+    export NIX_SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+
+    pushd vendor/bearssl/bearssl/certs
+    # Rebuild the cacert.c file to use the users custom certs
+    echo '#include <brssl.h>' > cacert.c
+    ${pkgs.bearssl}/brssl ta $NIX_SSL_CERT_FILE | sed "s/static //" >> cacert.c
+    # Update nix binding to point to correct amount of certificates
+    taNum=$(grep "#define TAs_NUM" cacert.c | awk '{print $NF}')
+    sed "s/const MozillaTrustAnchorsCount\* =.*/const MozillaTrustAnchorsCount* = $taNum/" -i cacert.nim
+    cat cacert.nim
+    popd
+  '';
+
   nimbleHash = "sha256-qaoVDxcYDZJG99TwK4IR8TxBAMJKEVzSmJNyFQt52iI=";
   nativeBuildInputs = [ ];
   doCheck = false;
